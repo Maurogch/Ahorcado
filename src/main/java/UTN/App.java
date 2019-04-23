243d5e0a74a1ae40b2cb44e5a2f5ec8d740dd77a
Mauro <maurogch@gmail.com>
2019-04-23 05:43:52 -0300

    Implemented daos calls, theads inicialization and storing of winner

--- src/main/java/UTN/App.java
+++ src/main/java/UTN/App.java
@@ -1,20 +1,48 @@
 package UTN;
 
+import UTN.Database.WinnerDAO;
+import UTN.Database.WordDAO;
 import UTN.Models.Player;
+import UTN.Models.Winner;
 import UTN.Models.Word;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Word word = new Word();
-        Player p = new Player(word, 10);
+        WordDAO wordDAO = new WordDAO();
+        WinnerDAO winnerDAO = new WinnerDAO();
+        Winner winner;
 
-        Thread p1 = new Thread(p);
-        Thread p2 = new Thread(p);
+        Word word = new Word(wordDAO.getWord());
+        Player p1 = new Player(word, 12, "Player 1");
+        Player p2 = new Player(word, 12, "Player 2");
+
+        /* Not necessary
+        /*Thread tPlayer1 = new Thread(p1);
+        Thread tPlayer2 = new Thread(p2);
+
+        tPlayer1.start();
+        tPlayer2.start();*/
 
         p1.start();
+        p2.start();
 
+        //Waits for thread to die
+        try {
+            p1.join();
+            p2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(p1.getWin()){
+            winner = new Winner(p1.getPlayerName(),word.getPlayedWord());
+            winnerDAO.setWinner(winner);
+        }else if(p2.getWin()){
+            winner = new Winner(p2.getPlayerName(),word.getPlayedWord());
+            winnerDAO.setWinner(winner);
+        }
 
 
     }
