49e3ab5a65a2daf9d44fdbf615cc26f0c4ea61d8
Mauro <maurogch@gmail.com>
2019-04-23 05:39:40 -0300

    Player and Word methods complete

--- src/main/java/UTN/Models/Player.java
+++ src/main/java/UTN/Models/Player.java
@@ -1,43 +1,79 @@
 package UTN.Models;
 
-import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class Player extends Thread{
+    private String playerName;
     private Word word;
     private int lives;
-    private Set<Character> playedChars = new HashSet<>();
+    private Boolean win;
 
-    public Player(Word word, int lives) {
+
+    public Player(Word word, int lives, String name) {
         this.word = word;
         this.lives = lives;
-    }
-
-    public Player(Word word) {
-        this.word = word;
+        this.playerName = name;
+        win = false;
     }
 
     @Override
     public void run(){
-        while (lives > 0){ //and the returned win bool
-            word.play(this.getChar());
+        while (lives > 0 && !word.gameEnded()){
+            try {
+                char playedChar = getChar();
+                System.out.println(playerName + " guessed with character: " + playedChar);
+                win = word.play(playedChar);
+                if(!win) lives--;
+            } catch (NullPointerException e) {
+                System.out.println("Error: No more letters to play");
+                System.exit(0);
+            }
+        }
+
+        if(lives == 0){
+            System.out.println("\n" + playerName + " has lost the game, correct word was " + word.getPlayedWord());
+        }
+
+        if(win){
+            System.out.println("\n" + playerName + " has won the game, correct word was -" + word.getPlayedWord() + "-");
+            System.out.println("Total number of tries : " + word.getPlayedChars().size());
         }
     }
 
+    //Gets list of played chars from word, then returns a char not yet played
     private char getChar(){
-        Boolean a = false;
+        boolean notPlayed = false;
         Character c = null;
         Random r;
+        char[] vocals = {'a', 'e', 'i', 'o', 'u'};
+        Set<Character> playedChars = word.getPlayedChars();
 
-        while (!a){
-            r = new Random();
-            c = (char)(r.nextInt(26) + 'a');
-            a = playedChars.add(c);
+        while (!notPlayed && playedChars.size() != 26){
+            //Try to play with vocals first
+            if(playedChars.size()<5) {
+                c = vocals[playedChars.size()];
+                notPlayed = playedChars.add(c);
+            }
+
+            //If all vocals played get a random char from the alphabet
+            if(!notPlayed) {
+                r = new Random();
+                c = (char) (r.nextInt(26) + 'a');
+                notPlayed = playedChars.add(c);
+            }
         }
+
+        word.setPlayedChars(playedChars);
 
         return c;
     }
 
+    public Boolean getWin() {
+        return win;
+    }
 
+    public String getPlayerName() {
+        return playerName;
+    }
 }
--- src/main/java/UTN/Models/Word.java
+++ src/main/java/UTN/Models/Word.java
@@ -1,9 +1,21 @@
 package UTN.Models;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Word {
     private String playedWord;
     private boolean win;
-    private boolean playing;
+    private boolean playing = false;
+    private boolean gameEnded = false;
+    private List<Character> correctChars = new ArrayList<>();
+    private Set<Character> playedChars = new HashSet<>();
+
+    public Word(String playedWord) {
+        this.playedWord = playedWord;
+    }
 
     public synchronized boolean play(char playedChar){
         while (playing){
@@ -12,15 +24,41 @@ public class Word {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
-            playing = true;
-
-            //todo play game
-
-            playing = false;
-            notify();
         }
 
+        if(gameEnded) return false; //This was necessary as both players would win otherwise, ask why
+        //Why was the other thread entering if the game ended was true after the noftify
+
+        playing = true;
+
+        if(playedWord.indexOf(playedChar) >= 0)
+            correctChars.add(playedChar);
+
+        if (playedWord.length() == correctChars.size()) {
+            win = true;
+            gameEnded = true;
+        }
+
+        playing = false;
+
+        notify();
+
         return win;
+    }
+
+    public synchronized Set<Character> getPlayedChars() {
+        return playedChars;
+    }
+
+    public synchronized void setPlayedChars(Set<Character> playedChars) {
+        this.playedChars = playedChars;
+    }
+
+    public String getPlayedWord() {
+        return playedWord;
+    }
+
+    public boolean gameEnded() {
+        return gameEnded;
     }
 }
