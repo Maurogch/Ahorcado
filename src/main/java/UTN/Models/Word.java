1c5731ff8e8e1c00edb8869ea8bef41c6bd82a29
maurogch <maurogch@gmail.com>
2019-04-28 22:50:39 -0300

    Fixed potential error with repeated character when guessing a word

--- src/main/java/UTN/Models/Word.java
+++ src/main/java/UTN/Models/Word.java
@@ -30,8 +30,13 @@ public class Word {
 
         playing = true;
 
+        /*Deprecated, if the character appeared more than once, it would only add it once
         if(playedWord.indexOf(playedChar) >= 0)
+            correctChars.add(playedChar);*/
+
+        for (int i = 0; i < countOccurences(playedWord, playedChar, 0); i++) {
             correctChars.add(playedChar);
+        }
 
         if (playedWord.length() == correctChars.size()) {
             win = true;
@@ -43,6 +48,16 @@ public class Word {
         notify();
 
         return win;
+    }
+
+    //Recursive method to count chars in a String
+    private static int countOccurences(String someString, char searchedChar, int index) {
+        if (index >= someString.length()) {
+            return 0;
+        }
+
+        int count = someString.charAt(index) == searchedChar ? 1 : 0;
+        return count + countOccurences(someString, searchedChar, index + 1);
     }
 
     public synchronized Set<Character> getPlayedChars() {
